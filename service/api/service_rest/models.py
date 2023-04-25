5164c088b849a1ce0591b2d08a64fc512ddf2014
Kevin <cedillokev20@gmail.com>
2023-04-25 17:01:00 -0400

    update

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class Technician(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    employee_id = models.CharField(max_length=100)
+
+class AutomobileVO(models.Model):
+    vin = models.CharField(max_length=100)
 # Create your models here.
