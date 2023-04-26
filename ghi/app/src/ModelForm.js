7f68109f16053324496f0673f26d6e13d142a8dc
Kevin <cedillokev20@gmail.com>
2023-04-26 15:57:11 -0400

    update

--- ghi/app/src/ModelForm.js
+++ ghi/app/src/ModelForm.js
@@ -41,7 +41,7 @@ const handleName = (event) => {
   }  
 return (
 <div className="shadow p-4 mt-4">
-        <header> Create A Vehicle</header>
+        <h1> Create A Vehicle</h1>
             <form onSubmit={handleSubmit} id="create-manufacturer-form">
             <div className="form-floating mb-3">
                 <label htmlFor="name">Name</label>
@@ -53,7 +53,7 @@ return (
                 </div>
                 <div className="form-floating mb-3">
                 <label htmlFor="manufacturer">Manufacturer</label>
-                <input onChange={handleManufacturer} placeholder="Manufacturer" required type="text" name="picture_url" id="picture_url" className="form-control"/>
+                <select onChange={handleManufacturer} placeholder="Manufacturer" required type="text" name="manufacturer" id="manufacturer_id" className="form-control"/>
                 </div>
             <button className="btn btn-primary">Create</button>
             </form>
@@ -62,18 +62,4 @@ return (
 }
 export default ModelForm
 
-        //  <form onSubmit={handleSubmit} id="create-model-form">
-        //  //   <div class="form-group">
-        //  //   <input onChange={handleName} placeholder="Name" required type="text" name="name" id="name" className="form-control"/>
-        //  //     <label htmlFor="name">Name</label>
-        //  //   </div>
-        //  //   <div class="form-group">
-        //  //   <input onChange={handlePictureUrl} placeholder="Picture Url" required type="text" name="picture_url" id="picture_url" className="form-control"/>
-        //  //     <label htmlFor="picture_url">Picture Url</label>
-        //  //   </div>
-        //  //   <div class="form-group">
-        //  //   <input onChange={handleManufacturer} placeholder="Manufacturer" required type="text" name="manufacturer" id="manufacturer" className="form-control"/>
-        //  //     <label htmlFor="manufacturer">Picture Url</label>
-        //  //   </div>
-        //  //   <button type="submit" class="btn btn-primary">Submit</button>
-        //  // </form>
+       
