78ff1c4bde17922420daefe3e5b6cdab1bd48d60
Jorge <landerosjorge8@outlook.com>
2023-06-05 21:19:13 -0700

    Inventory Automobile

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,18 +1,26 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
+import AutoForm from './AutomobileForm';
+import AutosList from './AutomobileList';
+import ManufacturerList from './ManufacturerList';
+import ManufacturerForm from './ManufacturerForm';
 
-function App() {
+function App(props) {
   return (
     <BrowserRouter>
       <Nav />
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="inventory/automobiles" element={<AutosList />} />
+          <Route path="inventory/automobiles/new" element={<AutoForm />} />
+          <Route path="inventory/manufacturers" element={<ManufacturerList manufacturers={props.manufacturers} />} />
+          <Route path="inventory/manufacturers/new" element={<ManufacturerForm />} />
         </Routes>
       </div>
     </BrowserRouter>
   );
 }
 
-export default App;
+  export default App;
--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -10,6 +10,20 @@ function Nav() {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink className="nav-link" aria-current="page" to="/">Home</NavLink>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" id="navbarInventoryDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Inventory
+              </a>
+              <ul className="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarInventoryDropdownMenuLink">
+                <li><NavLink className="dropdown-item" aria-current="page" to="inventory/automobiles">Automobiles List</NavLink></li>
+                <li><NavLink className="dropdown-item" aria-current="page" to="inventory/automobiles/new">Automobiles Form</NavLink></li>
+                <li><NavLink className="dropdown-item" aria-current="page" to="inventory/manufacturers">Manufacturer List</NavLink></li>
+                <li><NavLink className="dropdown-item" aria-current="page" to="inventory/manufacturers/new">Manufacturer Form</NavLink></li>
+              </ul>
+            </li>
           </ul>
         </div>
       </div>
